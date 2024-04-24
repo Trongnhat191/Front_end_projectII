e6b687eff601e8fdfc625f24f4e2a3c433761513
Trongnhat191 <nhat.nt215625@sis.hust.edu.vn>
2024-04-24 22:20:13 +0700

    24.04 #40

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,24 +2,21 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss'
-import { getAllUsers } from '../../services/userService';
+import { getAllUsers,createNewUserService } from '../../services/userService';
+import ModalUser from './ModalUser';
 
 class UserManage extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            arrUsers: []
+            arrUsers: [],
+            isOpenModalUser: false,
         }
     }
 
     async componentDidMount() {// goi api lay du lieu, state luu gia tri du lieu 
-        let response = await getAllUsers('ALL');
-        if (response && response.errCode === 0) {
-            this.setState({
-                arrUsers: response.users
-            })
-        }
+        await this.getAllUsersFromReact();
         //console.log('get user from Node.js:', response);
     }
 
@@ -29,25 +26,73 @@ class UserManage extends Component {
         2. did mount -> set state
         3. render
      */
+
+
+    getAllUsersFromReact = async () => {
+        let response = await getAllUsers('ALL');
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            })
+        }
+    }
+    //bat su kien OnClick button
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true,
+        })
+    }
+
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        })
+    }
+
+    createNewUser = async(data) => {
+        try {
+            let response = await createNewUserService(data);
+            if (response && response.errCode !==0){
+                alert(response.errMessage)
+            }else{
+                await this.getAllUsersFromReact();
+                this.setState({
+                    isOpenModalUser: false
+                })
+            }
+        } catch (e) {
+            console.log(e)
+        }
+        console.log('chceck data from child: ', data)
+    }
+
     render() {
-        console.log('check render', this.state)
         let arrUsers = this.state.arrUsers;
+        //props
         return (
             <div className="users-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFromParent={this.toggleUserModal}
+                    createNewUser={this.createNewUser}
+                />
                 <div className='title text-center'>Manage users with Eric</div>
+                <div className='mx-1'>
+                    <button className='btn btn-primary px-3' onClick={() => this.handleAddNewUser()}><i className="fas fa-plus"></i>Add new users</button>
+                </div>
                 <div className='users-table mt-4 mx-1'>
                     <table id="customers">
-                        <tr>
-                            <th>Email</th>
-                            <th>First name</th>
-                            <th>Last name</th>
-                            <th>Address</th>
-                            <th>Actions</th>
-                        </tr>
-                        
+                        <tbody>
+                            <tr>
+                                <th>Email</th>
+                                <th>First name</th>
+                                <th>Last name</th>
+                                <th>Address</th>
+                                <th>Actions</th>
+                            </tr>
+
                             {
                                 arrUsers && arrUsers.map((item, index) => {
-                                    console.log('eric check map', item, index)
                                     return (
                                         <tr>
                                             <td>{item.email}</td>
@@ -56,13 +101,13 @@ class UserManage extends Component {
                                             <td>{item.address}</td>
                                             <td>
                                                 <button><i className="fas fa-pencil-alt"></i></button>
-                                                <button><i className = "fas fa-trash"></i></button>
+                                                <button><i className="fas fa-trash"></i></button>
                                             </td>
                                         </tr>
                                     )
                                 })
                             }
-
+                        </tbody>
                     </table>
 
                 </div>
