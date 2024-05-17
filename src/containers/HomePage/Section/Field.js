d4fcac61674b08675a94202d9ff83b026fc2b929
Trongnhat191 <nhat.nt215625@sis.hust.edu.vn>
2024-05-17 20:53:19 +0700

    #49

--- src/containers/HomePage/Section/Field.js
+++ src/containers/HomePage/Section/Field.js
@@ -5,41 +5,56 @@ import './Field.scss';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import field1 from '../../../assets/field/field1.jpg';
 
 class Field extends Component {
-
     render() {
         let settings = {
             dots: false,
             infinite: true,
             speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            // nextArrow: <SampleNextArrow />,
+            // preArrow: <SamplePreArrow />
         };
         return (
             <div className='section-field'>
-                <div className='field-content'>
-                    <Slider {...settings}>
-                        <div className='img-customize'>
-                            <h3 >1</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>2</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>3</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>4</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>5</h3>
-                        </div>
-                        <div className='img-customize'>
-                            <h3>6</h3>
-                        </div>
+                <div className='field-container'>
+                    <div className='field-header'>
+                        <span className='title-section'>San pho bien</span>
+                        <button className='btn-section'>Xem them</button>
+                    </div>
+                    <div className='field-body'>
+                        <Slider {...settings}>
+                            <div className='field-customize'>
+                                <div className='bg-image' />
+                                <div>San 1</div>
+                            </div>
+                            <div className='field-customize'>
+                                <div className='bg-image' />
+                                <div>San 1</div>
+                            </div>
+                            <div className='field-customize'>
+                                <div className='bg-image' />
+                                <div>San 1</div>
+                            </div>
+                            <div className='field-customize'>
+                                <div className='bg-image' />
+                                <div>San 1</div>
+                            </div>
+                            <div className='field-customize'>
+                                <div className='bg-image' />
+                                <div>San 1</div>
+                            </div>
+                            <div className='field-customize'>
+                                <div className='bg-image' />
+                                <div>San 1</div>
+                            </div>
 
-                    </Slider>
+                        </Slider>
+                    </div>
+
                 </div>
             </div>
         );
